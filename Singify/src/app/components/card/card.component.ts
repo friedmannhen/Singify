67afc44137f29421c34dde4f48ade72c9e1dc714
Hen Friedmann <99195954+friedmannhen@users.noreply.github.com>
2025-02-23 14:37:45 +0200

    initial commit

---
+++ Singify/src/app/components/card/card.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { WordsService } from '../../services/words.service';
+import { word } from '../../models/word.interface';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-card',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  public word: word = { word: 'null', translation: 'null', used: false };
+  word1Hidden = false;
+  word2Hidden = false;
+  showTranslation = false;
+
+  constructor(private wordsService: WordsService) {}
+
+  ngOnInit(): void {
+    this.word = this.wordsService.getWord();
+  }
+  changeWord() {
+    this.word1Hidden = true;
+    this.word2Hidden = true;
+
+    setTimeout(() => {
+      this.word = this.wordsService.getWord();
+      this.word1Hidden = false;
+      this.word2Hidden = false;
+    }, 300);
+  }
+  toggleTranslation(event: Event) {
+    event.stopPropagation(); // Prevent event bubbling
+    this.showTranslation = !this.showTranslation;
+  }
+}
---
+++ Singify/src/app/models/word.interface.ts
@@ -0,0 +1,5 @@
+export interface word {
+  word: string;
+  translation: string;
+  used: boolean;
+}
